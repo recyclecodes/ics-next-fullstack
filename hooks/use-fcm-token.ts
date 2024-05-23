f999f9087c8af63c206a3a765be58f33b3d6bf73
Godfrey Anthony T. Serojales <gaserojales@gmail.com>
2024-05-23 14:33:47 +0800

    upadte: declared fcmtoken unique

--- hooks/use-fcm-token.ts
+++ hooks/use-fcm-token.ts
@@ -1,31 +1,31 @@
-"use client";
-import { useEffect, useState } from "react";
-import { getToken, isSupported } from "firebase/messaging";
-import useNotificationPermission from "./use-notification-permission";
-import { messaging } from "@/utils/firebase";
+    "use client";
+    import { useEffect, useState } from "react";
+    import { getToken, isSupported } from "firebase/messaging";
+    import useNotificationPermission from "./use-notification-permission";
+    import { messaging } from "@/utils/firebase";
 
 
-const useFCMToken = () => {
-  const permission = useNotificationPermission();
-  const [fcmToken, setFcmToken] = useState<string | null>(null);
+    const useFCMToken = () => {
+    const permission = useNotificationPermission();
+    const [fcmToken, setFcmToken] = useState<string | null>(null);
 
-  useEffect(() => {
-    const retrieveToken = async () => {
-      if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-        if (permission === "granted") {
-          const isFCMSupported = await isSupported();
-          if (!isFCMSupported) return;
-          const fcmToken = await getToken(messaging(), {
-            vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-          });
-          setFcmToken(fcmToken);
+    useEffect(() => {
+        const retrieveToken = async () => {
+        if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+            if (permission === "granted") {
+            const isFCMSupported = await isSupported();
+            if (!isFCMSupported) return;
+            const fcmToken = await getToken(messaging(), {
+                vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+            });
+            setFcmToken(fcmToken);
+            }
         }
-      }
+        };
+        retrieveToken();
+    }, [permission]);
+
+    return fcmToken;
     };
-    retrieveToken();
-  }, [permission]);
 
-  return fcmToken;
-};
-
-export default useFCMToken;
+    export default useFCMToken;
